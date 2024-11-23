398b3b2fae554625179d55ad9fb592c0baa3b3f9
Nguyễn Hoàng Lâm <165152517+Kuren20052002@users.noreply.github.com>
2024-11-23 23:37:55 +0700

    Add search capability

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,12 +1,25 @@
-class FlightsController < ApplicationController
-  def index
-    flights = Flight.upcoming
+  class FlightsController < ApplicationController
+    def index
+      airports = Airport.order(:code).all.map { |airport| [ airport.code, airport.id ] }
+      @departure_airport_options = airports
+      @arrival_airport_options = airports
 
-    @departure_airport_options = flights.map { |flight| [ flight.departure_airport.code, flight.departure_airport.id ] }.uniq
-    @arrival_airport_options = flights.map { |flight| [ flight.arrival_airport.code, flight.arrival_airport.id ] }.uniq
+      @start_dates = Flight.upcoming.pluck(:start_date).map { |date| [ date.strftime("%B %d, %Y"), date ] }.uniq
 
-    @start_dates = flights.pluck(:start_date).map { |date| [ date.strftime("%B %d, %Y"), date ] }.uniq
+      @passenger_numbers = (1..9).to_a
 
-    @passenger_numbers = (1..9).to_a
+      if search_params.present?
+        @flights = Flight.includes(:departure_airport, :arrival_airport)
+                         .where("departure_airport_id = ? AND arrival_airport_id = ? AND start_date >= ?",
+                                params[:search][:departure_airport_id], params[:search][:arrival_airport_id],
+                                params[:search][:start_date])
+        @passenger_number = params[:search][:passenger_number]
+      end
+    end
+
+    private
+
+    def search_params
+      params.fetch("search", {}).permit(:departure_airport_id, :arrival_airport_id, :start_date, :passenger_number)
+    end
   end
-end
