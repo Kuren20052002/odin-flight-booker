8e1056122ca1e5207452111ddb21499b2a071335
Nguyễn Hoàng Lâm <165152517+Kuren20052002@users.noreply.github.com>
2024-11-24 10:40:43 +0700

    Add Booking and Passenger model, associations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,7 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+  has_many :bookings, depedent: :destroy
   validate :departure_is_not_the_same_as_arrival
 
   scope :upcoming, -> { where(start_date: Time.now..) }
